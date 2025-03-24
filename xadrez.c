c2a61bf06350ef9dc997eb8c04034ff5f2010de7
DenisAraujo96 <denis.sousa1996@outlook.com>
2025-03-24 19:58:37 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,14 @@
 int main() {
   //mover a torre 5 casas para a direita
   for(int i = 0; i < 5; i++){
-    printf("direita\n");//imprime a direção do movimento
+    printf("Direita\n");//imprime a direção do movimento
   }
+  for (int i = 0; i < 5; i++){
+    printf("Cima Direita\n");//imprime a direção do movimento
+  }
+  for (int i = 0; i < 8; i++){
+    printf("Esquerda\n");
+  }
+  
     return 0;
 }
